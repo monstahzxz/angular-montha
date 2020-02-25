f12414b872e6da203e3874a245d45f447dbdc2c9
unknown <vimalsebastian4u@gmail.com>
2020-02-25 14:47:45 +0530

    proto

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from "../user";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  // ngOnInit(): void {
+  //   document.body.classList.add("back");
+  // }
+
+  userModel = new User("", "");
+  onSubmit() {
+    console.log(this.userModel);
+  }
+
+}
